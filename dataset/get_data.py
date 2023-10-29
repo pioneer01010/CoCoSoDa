84295eee45936193a1a8ea719aa257b914defd3a
pioneer01010 <pioneer020@gmail.com>
2023-10-29 14:28:24 +0900

    Modify get_data repo

---
+++ dataset/get_data.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+import kaggle
+
+path = Path("dataset")
+path.parent.mkdir(parents=True, exist_ok=True)
+kaggle.api.authenticate()
+kaggle.api.dataset_download_files("omduggineni/codesearchnet", path=path, unzip=False)
--- dataset/get_data.sh
+++ dataset/get_data.sh
@@ -1,9 +1,4 @@
-wget https://s3.amazonaws.com/code-search-net/CodeSearchNet/v2/python.zip
-wget https://s3.amazonaws.com/code-search-net/CodeSearchNet/v2/java.zip
-wget https://s3.amazonaws.com/code-search-net/CodeSearchNet/v2/ruby.zip
-wget https://s3.amazonaws.com/code-search-net/CodeSearchNet/v2/javascript.zip
-wget https://s3.amazonaws.com/code-search-net/CodeSearchNet/v2/go.zip
-wget https://s3.amazonaws.com/code-search-net/CodeSearchNet/v2/php.zip
+python get_data.py
 
 unzip python.zip
 unzip java.zip
